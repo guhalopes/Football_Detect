9c79c2cc3336befd04ea4ba55ea05120f3242d55
Guhalopes <gustavo.angioleti@gmail.com>
2020-04-28 11:37:37 -0300

    transforma o codigo em uma funcao que retorna a imagem em gray e thresh

--- pre_proc.py
+++ pre_proc.py
@@ -8,40 +8,35 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# read only one frame
-img = cv2.imread('football.png')
+# # read only one frame
+# img = cv2.imread('football.png')
 
-# convert to hsv image
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def preproc(img):
 
-# green range
-lower_green = np.array([40,40,40])
-upper_green = np.array([70,255,255])
-# blue range
-lower_blue = np.array([110,50,50])
-upper_blue = np.array([130,255,255])
-# red range
-lower_red = np.array([0,31,255])
-upper_red = np.array([176,255,255])
-# white range
-lower_white = np.array([0,0,0])
-upper_white = np.array([0,0,255])
-
-# define a mask with upper and lower values
-mask = cv2.inRange(hsv, lower_green, upper_green)
-
-# apply mask
-res = cv2.bitwise_and(img, img, mask=mask)
-
-# hsv to gray
-res_bgr = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
-res_gray = cv2.cvtColor(res_bgr, cv2.COLOR_BGR2GRAY)
-
-# define some morphological operations to reduce noise
-kernel = np.ones((13,13), np.uint8)
-thresh = cv2.threshold(res_gray, 127, 255, cv2.THRESH_BINARY_INV |  cv2.THRESH_OTSU)[1]
-thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
+    # convert to hsv image
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    # green range
+    lower_green = np.array([40,40,40])
+    upper_green = np.array([70,255,255])
+    
+    
+    # define a mask with upper and lower values
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    
+    # apply mask
+    res = cv2.bitwise_and(img, img, mask=mask)
+    
+    # hsv to gray
+    res_bgr = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+    res_gray = cv2.cvtColor(res_bgr, cv2.COLOR_BGR2GRAY)
+    
+    # define some morphological operations to reduce noise
+    kernel = np.ones((13,13), np.uint8)
+    thresh = cv2.threshold(res_gray, 127, 255, cv2.THRESH_BINARY_INV |  cv2.THRESH_OTSU)[1]
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    
+    return res_gray, thresh
 
 
 
@@ -51,7 +46,7 @@ thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 # plt.show()
 # plt.close()
 
-# plot multiple images
+# # plot multiple images
 # plt.subplots(1, 3, figsize=(20, 15))
 
 # plt.subplot(1, 3, 1), plt.imshow(img)
